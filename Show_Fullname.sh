202c3538514d4a0b7ef5adc0013a101ad5291df9
Shampilelee <64449513+Shampilelee@users.noreply.github.com>
2022-04-21 02:05:25 +0000

    Add files via upload

---
+++ Show_Fullname.sh
@@ -0,0 +1,48 @@
+#!/bin/zsh
+
+echo ""
+echo ""
+echo ""
+echo "                      oooooooooooo  o   o  oooooooooooo "
+echo "                           |         o o        |        "
+echo "                           |          o         |        "
+echo "                           |          |         |        "
+echo "                                              SOFTWARES \n\n"
+
+
+
+sleep 2
+
+echo "Get Day, Enter Your First Name: "
+read userFirstName
+
+sleep 1
+
+echo "\nEnter Your Middle Name: "
+read userMiddleName
+
+sleep 1
+
+echo "\nEnter Your Last Name: "
+read userLastName
+
+sleep 1
+
+echo "\n          ||||||||||  GENERATION YOUR FULLNAME |||||||||| \n"
+
+sleep 5
+
+echo "  [5]. \n"
+sleep 4
+echo "  [4].. \n"
+sleep 3
+echo "  [3]... \n"
+sleep 2
+echo "  [2].... \n"
+sleep 1
+echo "  [1]..... \n\n\n"
+
+
+echo "                        [[ FOUND YOUR FULL NAME ]]   "
+sleep 1
+echo "         USER FULLNAME: [ $userLastName $userMiddleName $userFirstName ]  "
